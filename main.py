ffbbee0ff3fe8072d2ea8aa88b90113213fe31b2
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2023-07-24 22:52:50 +0400

    chat_id,

--- main.py
+++ main.py
@@ -29,16 +29,16 @@ async def start_command(_, message):
 async def download_file(client, message):
     chat_id = message.chat.id
 
-    dcode_msg = await client.ask("Enter the download code:", filters=filters.text)
+    dcode_msg = await client.ask(chat_id, "Enter the download code:", filters=filters.text)
     dcode = dcode_msg.text
 
-    resolution_msg = await client.ask("Enter the resolution (e.g., 1080p, 720p, 480p, 360p):", filters=filters.text)
+    resolution_msg = await client.ask(chat_id, "Enter the resolution (e.g., 1080p, 720p, 480p, 360p):", filters=filters.text)
     resolution = resolution_msg.text
 
-    format_msg = await client.ask("Enter the file format (e.g., mp4, mkv (if you need softcoded subtitles)):", filters=filters.text)
+    format_msg = await client.ask(chat_id, "Enter the file format (e.g., mp4, mkv (if you need softcoded subtitles)):", filters=filters.text)
     file_format = format_msg.text
 
-    filename_msg = await client.ask("Enter the filename:", filters=filters.text)
+    filename_msg = await client.ask(chat_id, "Enter the filename:", filters=filters.text)
     filename = filename_msg.text
 
     await message.reply_text("Downloading the file... Please wait...")
